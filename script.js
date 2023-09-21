81fcfe1b9708ce8a14c50f10c8b8f9b145baba55
Welinghtonn <126928676+Welinghtonn@users.noreply.github.com>
2023-09-21 18:28:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
 var array = [];
+array = {
+    id: 1,
+    name: valorInput,
+    del: Boolean
+}
+
+const list = document.querySelector(".lista")
 
 function Lista() {
     var valorInput = document.getElementById("valor").value;
-    array.push(valorInput);
-    if(valorInput === ""){
+    if (valorInput === "") {
+        return
+    } else {
+        list.innerHTML = ""
+        array.push(valorInput);
+        array.forEach((item) => {
+            let container = document.createElement("div");
+            let checkbox = document.createElement("input");
+            let p = document.createElement("p");
+            let button = document.createElement("button");
+            const del = document.createElement("del");
 
-    }else{
-        document.getElementById("root").innerHTML = array.map(item => 
-            `<p>
-                <input type="checkbox">
-                ${item}
-                <button onclick="Remover()"><ion-icon class="remover"  name="trash-outline"></ion-icon></button>
-            </p>`).join("");
+            container.className = "container";
+            checkbox.type = "checkbox";
+            checkbox.addEventListener("change", () => {
+                console.log(`Check no item: ${item}`);
+                if(checkbox.checked === true){
+                    container.style.backgroundColor = "green"
+                }else{
+                    container.style.backgroundColor = "white"
+                }
+            });
+
+            checkbox.className = "chek"
+            button.className = "delete";
+            p.innerText = item;
+            del.innerText = item;
+            p.className = "text"
+
+            container.append(checkbox, p, button);
+            list.appendChild(container);
+
+            button.addEventListener('click', function handleClick() {
+                container.innerHTML = ""
+                array = []
+
+            });
+        })
     }
 }
-
-function Remover() {
-    document.getElementById("root").innerHTML = array = []; 
-}
-
-
-
-
 
 
